61982adeb1e6bbb6c57e301b7d2e847ae35b238b
bboortz <benjamin.boortz@web.de>
2015-08-26 16:30:49 +0000

    build updated /BB

--- build_generic.sh
+++ build_generic.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
-echo "script not finished!!"
-exit
 
 # see http://null-byte.wonderhowto.com/how-to/create-custom-arch-linux-distro-0131002/
+CHROOT=./target/chroot2
+TARGET=${CHROOT%/*}
+
+# move to target directory
+mkdir -p ${TARGET}
+cd ${TARGET}
 
 # install software
 sudo pacman -S devtools git make --needed
-# Create a chroot with base-packages:
-mkarchroot /tmp/chroot base
+
 # Clone the Archiso package:
 git clone git://projects.archlinux.org/archiso.git
+# clone the arch-install-scripts package:
+# git clone https://github.com/falconindy/arch-install-scripts
+git clone git://projects.archlinux.org/arch-install-scripts.git
+
+# Create a chroot with base-packages:
+mkarchroot $CHROOT base-devel
+
 # Compile Archiso to your chroot:
-make -C archiso/archiso DESTDIR=/tmp/chroot 
-sudo make -C archiso/archiso DESTDIR=/tmp/chroot install
-# clone the arch install scripts
-git clone https://github.com/falconindy/arch-install-scripts
-make -C  arch-install-scripts DESTDIR=/tmp/chroot 
-sudo make -C  arch-install-scripts DESTDIR=/tmp/chroot install
+sudo make -C archiso DESTDIR=$CHROOT install
+# Compile arch-install-scripts to your chroot:
+sudo make -C arch-install-scripts DESTDIR=$CHROOT install
+
 # Enter our chroot:
-mkarchroot -r bash /tmp/chroot
+#mkarchroot -r bash $CHROOT
 
